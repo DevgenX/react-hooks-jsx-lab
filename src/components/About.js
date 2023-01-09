ba801b939b84b3eb51043e40d36dc871d00eb053
devgenx <sebastian110420@gmail.com>
2023-01-09 13:51:27 -0500

    commit tree 0aeafa256c12d70fa6d1b57255b66e87eec26eb9

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { image } from "../data/data";
 
-function About() {
-  return <div>About</div>;
-}
+const About = () => {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Nothin'</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
+};
+
+// function About() {
+//   return <div>About</div>;
+// }
 
 export default About;
